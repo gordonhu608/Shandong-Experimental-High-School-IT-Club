62f0bf602e6e0b1c00fa48c609669c7c83015e05
Amy <1179140002@qq.com>
2017-02-28 19:04:40 +0800

    Create Amy Huang.java

---
+++ Tasks and Projects/Tasks/Task 0- Learn to use Github (Hello World)/Amy Huang.java
@@ -0,0 +1,13 @@
+
+public class ITClubDemo {
+	public static void main(String[] args) {
+		String name = "Amy";
+		int className = 24;
+		System.out.println("I am " + name + " in class "+ className);
+		System.out.print("Hello World!");
+	}
+}
+
+	
+
+	
