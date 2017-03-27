779cc4f86ce307d52624bd9785692863a06c1c69
Gordon <gordonhu608@yahoo.com>
2017-03-27 22:18:49 +0800

    Create Gordon Hu .java

---
+++ Tasks and Projects/Tasks/Task 4 - IF ELSE statement/Gordon Hu .java
@@ -0,0 +1,15 @@
+public static void main(String[] args) { 
+2 	 
+3 	      int num1 = 3; 
+4 	      int num2 = 6; 
+5 	       
+6 	if (num1 > num2) 
+7 	  { 
+8 	     System.out.println("num1 is larger"); 
+9 	  } 
+10 	else 
+11 	  { 
+12 	     System.out.println("num 1 is larger"); 
+13 	  } 
+14 	   
+15 	} 
